d99cd2bdf56fb3d640825f39864a1d55bb0c0c2a
Itdesu <okumura.itto.xt@mynavi.jp>
2024-05-15 06:53:26 +0000

    Finish Day2

---
+++ Okumura/Day2/10_population.py
@@ -0,0 +1,15 @@
+world = ("China", "India", "U.S.A", "Indonesia", "Pakistan", "Brazil", "Nigeria", "Bangladesh", "Russia", "Mexico")
+
+search = int(input()) - 1
+
+print(world[search], end="")
+
+#---------------------------
+#import sys
+#args = sys.argv
+#
+#world = ("China", "India", "U.S.A", "Indonesia", "Pakistan", "Brazil", "Nigeria", "Bangladesh", "Russia", "Mexico")
+#
+#search = int(args[1]) - 1
+#
+#print(world[search], end="")
---
+++ Okumura/Day2/list-delete.py
@@ -0,0 +1,5 @@
+nums = [0, 1, 2, 3, 4]
+del nums[2]
+
+nums = [0, 1, 2, 3, 4]
+del nums[2:4]
